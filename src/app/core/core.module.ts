44018cc0a20f65c0c363ee30f986e51e7bde0eaa
Samaneh Basmechi <business.samanehbasmechi@gmail.com>
2021-03-13 21:20:38 +0330

    Add Header.

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,9 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
-
-
-
 @NgModule({
   declarations: [HeaderComponent],
   imports: [
